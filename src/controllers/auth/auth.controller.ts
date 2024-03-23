2bcd71baf0cc6a62c7d7ff29469ee4ade14e61b5
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-03-22 21:37:05 -0300

    middleware

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -55,7 +55,7 @@ export default class AuthController {
     })
   }
 
-  /*static async refresh (req: Request, res: Response) {
+  static async refresh (req: Request, res: Response) {
     const { authorization } = req.headers
 
     if (!authorization) return res.status(400).json({ error: 'O refresh token é obrigatório' })
@@ -63,13 +63,12 @@ export default class AuthController {
     const token = await Token.findOneBy({ refreshToken: authorization })
     if (!token) return res.status(401).json({ error: 'Refresh token inválido' })
 
-    // Verifica se o refresh token ainda é válido
     if (token.expiresAt < new Date()) {
       await token.remove()
       return res.status(401).json({ error: 'Refresh token expirado' })
     }
 
-    // Atualiza os tokens
+
     token.token = bcrypt.hashSync(Math.random().toString(36), 1).slice(-20)
     token.refreshToken = bcrypt.hashSync(Math.random().toString(36), 1).slice(-20)
     token.expiresAt = new Date(Date.now() + 60 * 60 * 1000)
@@ -82,7 +81,7 @@ export default class AuthController {
     })
   }
 
-  static async logout (req: Request, res: Response) {
+  /*static async logout (req: Request, res: Response) {
     const { authorization } = req.headers
     
     if (!authorization) return res.status(400).json({ error: 'O token é obrigatório' })
