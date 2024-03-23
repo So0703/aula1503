712d14bcbaecc9192d228645eea431666f639017
Sabrina Onishi <sabrina.m.onishi@hotmail.com>
2024-03-22 21:51:38 -0300

    Auth.routes

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -3,7 +3,9 @@ import AuthController from '../../controllers/auth/auth.controller'
 
 const authRoutes = Router()
 
-authRoutes.post('/register',AuthController.store)
-authRoutes.post('/login',AuthController.login)
+authRoutes.post('/register', AuthController.store)
+authRoutes.post('/login', AuthController.login)
+authRoutes.post('/refresh', AuthController.refresh)
+authRoutes.post('/logout', AuthController.logout)
 
 export default authRoutes
